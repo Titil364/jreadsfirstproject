26e178f5ae16ff6099fa658aaef985b9c7c166b5
Alexandre <alexlevoileux@orange.fr>
2017-04-06 16:00:39 +0100

    Tables modifications

--- docs/db.sql
+++ docs/db.sql
@@ -1,78 +1,174 @@
-/*CREATE TABLE Visitor(
-    visitorId int PRIMARY KEY,
-    visitorGroupId int,
-    visitorSecretName varchar(20),
-    visitorSchool varchar(20),
-    visitorAge int,
-    visitorClass varchar(20)
-);
-CREATE TABLE User(
-    userId int PRIMARY KEY,
-    userMail varchar(20),
-    userSurname varchar(20),
-    userForename varchar(20)
-);*/
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost
+-- Généré le :  Jeu 06 Avril 2017 à 16:58
+-- Version du serveur :  10.1.21-MariaDB
+-- Version de PHP :  5.6.30
 
-CREATE TABLE Form(
-    formId int PRIMARY KEY,
-    formName varchar(20),
-    userId int,
-    CONSTRAINT fk_user_form
-        FOREIGN KEY (userId) REFERENCES User(id)
-);
-
-CREATE TABLE Application(
-    applicationId int PRIMARY KEY,
-    applicationName varchar(20),
-    applicationDescription varchar(20),
-    formId int,
-    CONSTRAINT fk_form_application
-        FOREIGN KEY (formId)
-        REFERENCES Form(formId)
-);
-
-CREATE TABLE Question(
-    questionId int PRIMARY KEY,
-    questionName varchar(20),
-    applicationId int,
-    CONSTRAINT fk_application_question
-        FOREIGN KEY (applicationId)
-        REFERENCES Application(applicationId)
-);
-
-CREATE TABLE Answer(
-    visitorId int,
-    questionId int,
-    CONSTRAINT pk_answer PRIMARY KEY (visitorId, questionId);
-    CONSTRAINT fk_question_answer
-        FOREIGN KEY (questionId)
-        REFERENCES Question(questionId)
-    CONSTRAINT fk_visitor_answer
-        FOREIGN KEY (visitorId)
-        REFERENCES Visitor(visitorId)    
-);
-
-CREATE TABLE date(
-    visitorId int,
-    formId int,
-    CONSTRAINT pk_answer PRIMARY KEY (visitorId, formId);
-    CONSTRAINT fk_form_date
-        FOREIGN KEY (formId)
-        REFERENCES Form(formId)
-    CONSTRAINT fk_visitor_date
-        FOREIGN KEY (visitorId)
-        REFERENCES Visitor(visitorId)    
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `ChiCI`
+--
 
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `Answer`
+--
 
+CREATE TABLE `Answer` (
+  `visitorId` int(11) DEFAULT NULL,
+  `questionId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `Application`
+--
 
+CREATE TABLE `Application` (
+  `applicationId` int(11) NOT NULL,
+  `applicationName` varchar(20) DEFAULT NULL,
+  `applicationDescription` varchar(20) DEFAULT NULL,
+  `formId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `Form`
+--
 
+CREATE TABLE `Form` (
+  `formId` int(11) NOT NULL,
+  `formName` varchar(20) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Question`
+--
+
+CREATE TABLE `Question` (
+  `questionId` int(11) NOT NULL,
+  `questionName` varchar(20) DEFAULT NULL,
+  `applicationId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE `User` (
+  `userId` int(11) NOT NULL,
+  `userMail` varchar(20) DEFAULT NULL,
+  `userSurname` varchar(20) DEFAULT NULL,
+  `userForename` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Visitor`
+--
+
+CREATE TABLE `Visitor` (
+  `visitorId` int(11) NOT NULL,
+  `visitorGroupId` int(11) DEFAULT NULL,
+  `visitorSecretName` varchar(20) DEFAULT NULL,
+  `visitorSchool` varchar(20) DEFAULT NULL,
+  `visitorAge` int(11) DEFAULT NULL,
+  `visitorClass` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `Answer`
+--
+ALTER TABLE `Answer`
+  ADD KEY `fk_question_answer` (`questionId`),
+  ADD KEY `fk_visitor_answer` (`visitorId`);
+
+--
+-- Index pour la table `Application`
+--
+ALTER TABLE `Application`
+  ADD PRIMARY KEY (`applicationId`),
+  ADD KEY `fk_for_application` (`formId`);
+
+--
+-- Index pour la table `Form`
+--
+ALTER TABLE `Form`
+  ADD PRIMARY KEY (`formId`),
+  ADD KEY `fk_user_form` (`userId`);
+
+--
+-- Index pour la table `Question`
+--
+ALTER TABLE `Question`
+  ADD PRIMARY KEY (`questionId`),
+  ADD KEY `fk_application_question` (`applicationId`);
+
+--
+-- Index pour la table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- Index pour la table `Visitor`
+--
+ALTER TABLE `Visitor`
+  ADD PRIMARY KEY (`visitorId`);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Answer`
+--
+ALTER TABLE `Answer`
+  ADD CONSTRAINT `fk_question_answer` FOREIGN KEY (`questionId`) REFERENCES `Question` (`questionId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_visitor_answer` FOREIGN KEY (`visitorId`) REFERENCES `Visitor` (`visitorId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Application`
+--
+ALTER TABLE `Application`
+  ADD CONSTRAINT `fk_for_application` FOREIGN KEY (`formId`) REFERENCES `Form` (`formId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Form`
+--
+ALTER TABLE `Form`
+  ADD CONSTRAINT `fk_user_form` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Question`
+--
+ALTER TABLE `Question`
+  ADD CONSTRAINT `fk_application_question` FOREIGN KEY (`applicationId`) REFERENCES `Application` (`applicationId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
