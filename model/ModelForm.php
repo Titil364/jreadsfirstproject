b93d585941c88791d2a658485e90232863a1ba65
ClementManniez <clement.m34@gmail.com>
2017-04-06 16:50:52 +0100

    application form and visitor models added

---
+++ model/ModelForm.php
@@ -0,0 +1,96 @@
+<?php
+require_once File::build_path(array('model', 'Model.php'));
+
+class ModelForm extends Model{
+
+	private formId;
+	private formName;
+	private userId;
+
+    public function getFormId() {
+   		return $this->formId;
+    }    
+
+    public function getFormName() {
+   		return $this->formName;
+    }       
+
+    public function setFormId($formId) {
+   		$this->formId = $formId;
+    }    
+
+    public function setFormName($formName){
+   		$this->formName = $formName;
+    }
+
+    public function getUserId(){
+    	return $this->userId;
+    }
+
+    public function setUserId($userId){
+    	this->userId ->userId;
+    }
+
+
+    public function __construct( $formID= NULL, $formName = NULL, $userId = NULL) {
+        if (!is_null($formID) && !is_null($formName) && !is_null($userId)) {
+        	$this->formId = $formId;
+        	$this->formName = $formName;
+        	$this-> $userId = $userId;
+  	
+        }
+    }
+
+    public function getFormById($id){
+		try{
+			$sql  = "SELECT * FROM Form WHERE formId=:id";
+			$prep = Model::$pdo->prepare($sql);
+
+			$values = array(
+				"id" => $id,
+				);
+
+			$prep-> execute($values);
+			$prep->setFetchMode(PDO::FETCH_CLASS,'ModelForm');
+			$form_array = $prep->fetchAll();
+			
+			return $form_array[0];
+
+		}catch (PDOException $ex) {
+            if (Conf::getDebug()) {
+                echo $ex->getMessage();
+            } else {
+                echo "Error";
+            }
+            return false;
+        }
+    }
+
+    public function getFormByUserId($userId){
+		try{
+			$sql  = "SELECT * FROM Form WHERE formId=:id";
+			$prep = Model::$pdo->prepare($sql);
+
+			$values = array(
+				"id" => $userId,
+				);
+
+			$prep-> execute($values);
+			$prep->setFetchMode(PDO::FETCH_CLASS,'ModelForm');
+			$form_array = $prep->fetchAll();
+			
+			return $form_array;
+
+		}catch (PDOException $ex) {
+            if (Conf::getDebug()) {
+                echo $ex->getMessage();
+            } else {
+                echo "Error";
+            }
+            return false;
+        }
+    }    
+       
+
+
+
---
+++ model/ModelVisitor.php
@@ -0,0 +1,132 @@
+<?php
+require_once File::build_path(array('model', 'Model.php'));
+
+
+class ModelVisitor extends Model{
+	private visitorId;
+	private visitorGroupId;
+	private visitorName;
+	private visitorSecretName;
+	private visitorSchool;
+	private visitorAge;
+	private visitorClass;
+
+    protected static $object = "Visitor";
+    protected static $primary = 'visitorSecretName';
+
+    public function getVisitorId() {
+   		return $this->visitorId;
+    }    
+
+    public function getVisitorGroupId() {
+   		return $this->visitorGroupId;
+    }
+
+    public function getVisitorName(){
+    	return $this->visitorName;
+    }
+
+    public function getVisitorSecretName(){
+    	return $this->visitorSecretName;
+    }
+	
+    public function getVisitorSchool(){
+    	return $this->visitorSchool;
+    }
+	
+    public function getVisitorAge(){
+    	return $this->visitorAge;
+    }
+
+    public function getVisitorClass(){
+    	return $this->visitorClass;
+    }
+
+    public function setVisitorId($visitorGroupId) {
+   		$this->visitorId = $visitorId ;
+    }
+
+    public function setVisitorGroupId($visitorGroupId) {
+   		$this->visitorGroupId = $visitorGroupId ;
+    }
+
+    public function setVisitorName($visitorName){
+    	$this->visitorName = $visitorName ;
+    }
+
+    public function setVisitorSecretName($visitorSecretName){
+    	$this->visitorSecretName = $visitorSecretName ;
+    }
+	
+    public function setVisitorSchool($visitorSchool){
+    	$this->visitorSchool = $visitorSchool ;
+    }
+	
+    public function setVisitorAge($visitorAge){
+    	$this->visitorAge = $visitorAge ;
+    }
+
+    public function setVisitorClass($visitorClass){
+    	$this->visitorClass = $visitorClass ;
+    }
+
+    public function __construct( $visitorGroupId= NULL, $visitorName = NULL, $visitorSecretName = NULL, $visitorSchool = NULL, $visitorAge = NULL, $visitorClass = NULL) {
+        if (!is_null($visitorGroupId) && !is_null($visitorName) && !is_null($visitorSecretName) && !is_null($visitorSchool) && !is_null($visitorAge) && !is_null($visitorClass)) {
+
+        	$this->visitorGroupId = $visitorGroupId;
+			$this->visitorName = $visitorName;
+			$this->visitorSecretName = $visitorSecretName;
+			$this->visitorSchool = $visitorSchool;
+			$this->visitorAge = $visitorAge;
+			$this->visitorClass = $visitorClass;        	
+        }
+    }
+
+	public static function checkExistingVisitor($visitorSecretName){
+		try{	
+			$sql = "SELECT COUNT(*) FROM Visitors WHERE visitorSecretName=:visitorSecretName";
+			$prep = Model::$pdo->prepare($sql);
+			$values = array(
+				':visitorSecretName' => $visitorSecretName
+			);
+			$prep -> execute($values);
+			$prep -> setFetchMode(PDO::FETCH_NUM);
+			$result = $prep->fetchAll();
+			if($result[0][0]>=1){
+				return 1;
+			}else{
+				return 0;
+			}
+		} catch (PDOException $ex) {
+            if (Conf::getDebug()) {
+                echo $ex->getMessage();
+            } else {
+                echo "Error";
+            }
+            return false;
+        }
+	}
+
+	public static function findVisitorById($id){
+		try{
+			$sql  = "SELECT * FROM Visitors WHERE visitorId=:id";
+			$prep = Model::$pdo->prepare($sql);
+
+			$values = array(
+				"id" => $id,
+				);
+
+			$prep-> execute($values);
+			$prep->setFetchMode(PDO::FETCH_CLASS,'ModelVisitor');
+			$visitor_array = $prep->fetchAll();
+			
+			return $visitor_array[0];
+		}catch (PDOException $ex) {
+            if (Conf::getDebug()) {
+                echo $ex->getMessage();
+            } else {
+                echo "Error";
+            }
+            return false;
+        }
+	}
