c0e39c7c1f1587c6e7d87f5f62174e76438dcfe3
ClementManniez <clement.m34@gmail.com>
2017-04-11 17:03:01 +0100

    ajout controller answerType

---
+++ controller/ControllerAnswerType.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once File::build_path(array('model', 'ModelAnswerType.php'));
+
+class ControllerAnswerType {
+    public static function created(){
+        $view = 'index';
+        $controller = 'default';
+        $pagetitle = 'Create Form';
+        require File::build_path(array('view', 'view.php'));
+    }
+	
+    
+}
