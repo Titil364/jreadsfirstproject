b24711588c9b8c0f3def296e6ef384ab5a14ece6
ClementManniez <clement.m34@gmail.com>
2017-04-13 10:21:45 +0100

    corrected terrible ; mistakes :D

--- model/ModelFSQuestion.php
+++ model/ModelFSQuestion.php
@@ -8,10 +8,10 @@ class ModelFSQuestion extends Model{
     protected static $object = "FSQuestion";
     protected static $primary = 'FSQuestionId';
     
-    public function getFSQuestionId(){return $this->FSQuestionId};
+    public function getFSQuestionId(){return $this->FSQuestionId;}
     public function setFSQuestionId($FSQuestionId){$this->FSQuestionId = $FSQuestionId;} 
     
-    public function getFSQuestionName(){return $this->FSQuestionName};
+    public function getFSQuestionName(){return $this->FSQuestionName;}
     public function setFSQuestionName($FSQuestionName){$this->FSQuestionName = $FSQuestionName;} 
 
     public function __construct ($FSQuestionId = NULL, $FSQuestionName = NULL){
