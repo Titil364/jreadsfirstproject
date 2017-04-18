9919cd4a84fc282289bebb1178415c0208aceb27
Titil364 <ravetoute@gmail.com>
2017-04-19 00:01:49 +0200

    Correction validation

--- script/myscript.js
+++ script/myscript.js
@@ -504,12 +504,10 @@ function extractData(){
 		//console.log("desc "+applicationDesc);
 		//console.log("img "+applicationImg);
 		
-		if(applicationName === "" | (applicationDesc === "" & applicationImg === "")){
+		if(applicationName === "" | (applicationDesc === "" & $("#"+applicationImg).val() === "")){
 			alert("At least one application is not fully completed. Please check and add a description or image and a title. ");
-			console.log("nique");
 			return null;
 		}
-		//console.log("bug1 ");
 		var questions = $("#"+id+" > .question");
 		for(var y = 0; y < questions.length; y++){
 			//Dig out the type of the question (the radio button checked)
