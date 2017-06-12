4e878665dccdb8ce478abafaf4572e4ad81243a7
ClementManniez <clement.m34@gmail.com>
2017-06-12 22:35:45 +0100

    trying to fix pdf pb, need to check

--- controller/ControllerForm.php
+++ controller/ControllerForm.php
@@ -1066,7 +1066,7 @@ class ControllerForm {
 
         if (file_exists($file)) {
             header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
+            header('Content-Type: application/pdf');
             header('Content-Disposition: attachment; filename="'.basename($file).'"');
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
